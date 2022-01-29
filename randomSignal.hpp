f8a7bf3d75884b404f0d50066360940f4e3d387f
Bmois <lye11@u.rochester.edu>
2022-01-29 14:51:38 +0800

    Finished the basics of Diff and CumNormalized implementation

--- randomSignal.hpp
+++ randomSignal.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <algorithm>
 #include <cmath>
 #include <cstdlib>
 #include <time.h>
+#include <iostream>
 
 static int x = 0;
 class randomSignal {
@@ -11,11 +13,14 @@ public:
   randomSignal() {
     srand(time(NULL) % 1000);
     hz = rand() % (442 - 88) + 88; // 100HZ ~ 500HZ
+    printf("\n[INFO]========Random Signal hz is:\x1B[1;31m%i\033[0m\n", hz);
     amplitude = rand() % 801 + 200;
   }
 public:
-  long operator()() {
+  long operator()(int sampleRate = 44100) {
+    int tau = sampleRate / hz;
     // printf("x value generate with: %f \n", sin(2 * M_PI / 10 * (x)));
-    return amplitude * sin(2 * M_PI / hz * (x++)); // 440hz
+    // return amplitude * sin(2 * M_PI / hz * (x++)); // x is hz cycle
+    return amplitude * sin(2 * M_PI / tau * (x++)); // x is tau cycle
   }
 };
--- yin.hpp
+++ yin.hpp
@@ -1,33 +1,50 @@
 #pragma once
 #include <vector>
 
+/* 
+Implementation Approach:
+  - Generate taus and their errors, store into vector
+  - Search in the vector to Find the best tau
+*/
 class YIN {
 public:
   YIN();
-  YIN(int sr, unsigned int min_tau, unsigned int max_tau, int ws);
-  YIN(int sr, unsigned int min_tau, unsigned int max_tau, int ws,
+  YIN(int sr, unsigned int min_freq, unsigned int max_freq, int ws, double thrs);
+  YIN(int sr, unsigned int min_freq, unsigned int max_freq, int ws, double thrs, 
       std::vector<long> data);
 
 public:
   void setSR(int sr);
   void setWS(int ws);
   void setData(std::vector<long> signalData);
-  void setMaxTau(unsigned int maxtau);
-  void setMinTau(unsigned int mintau);
+  void setMaxFreq(unsigned int maxfreq);
+  void setMinFreq(unsigned int minfreq);
+  void setThreshold(double thrs);
   int getSR();
   std::vector<long> getData();
+  unsigned int getMinFreq();
+  unsigned int getMaxFreq();
   unsigned int getMinTau();
   unsigned int getMaxTau();
   int getWS();
+  double getThreshold();
+  double getFreq(std::vector<double> &data, double threshold = -1);
 
-  std::vector<double> differenceFunction(std::vector<long> *data, int W);
-  std::vector<double> cumulativeMeanNormalized(std::vector<long> *data);
-  std::vector<std::vector<long>> segmentData(int W);
+
+  std::vector<double> differenceFunction(std::vector<long> &data, int W = -1);
+  std::vector<double> cumulativeMeanNormalized(std::vector<double> &data);
+  std::vector<std::vector<long>> segmentData(int W = -1);
 
   std::vector<std::vector<double>> getPitches();
 
+  bool checkWindowSize(int ws = -1);
+  void updateTaus();
+
 private:
   int sampleRate;
+  double threshold;
+  unsigned int min_freq;
+  unsigned int max_freq;
   unsigned int min_tau;
   unsigned int max_tau;
   int windowSize;
